c3dcb81aa74928e5018076b30c85f0cf8b460ca1
Etibar <etibar.rustem@gmail.com>
2016-05-12 14:00:25 +0400

    check

--- js/12.05.16/js/main.js
+++ js/12.05.16/js/main.js
@@ -1,5 +1,12 @@
-var abc = "#";
-for (var i=0;i<15;i++){
-  document.write(abc+"<br>");
-  abc=abc+"#";  
+// var abc = "#";
+// for (var i=0;i<15;i++){
+//   document.write(abc+"<br>");
+//   abc=abc+"#";  
+// }
+var a ="#";
+for (var i=0;i<5;i++){
+	a="#"+"&nbsp;"+"&nbsp;"+"#"+"&nbsp;"+"&nbsp;"+"#"+"&nbsp;"+"&nbsp;"+"#";
+	document.write(a+"<br>");
+	a="&nbsp;"+"&nbsp;"+"#"+"&nbsp;"+"&nbsp;"+"#"+"&nbsp;"+"&nbsp;"+"#"+"&nbsp;"+"&nbsp;"+"#";
+	document.write(a+"<br>");
 }
