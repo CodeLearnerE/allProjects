2083652e3f872c36795151bf37585d93842a9e37
Etibar <etibar.rustem@gmail.com>
2016-05-16 11:42:44 +0400

    16 may

---
+++ js/05/js/script.js
@@ -0,0 +1,152 @@
+
+/*
+!true || !false ---true;
+(true && !false) && (false || !true) --false;
+(4 > 5) || true ---false
+document.write(("this" != "that") && ("zebra" > "aardvark")) ---?
+((4 >= 6) || ("grass" != "green")) && !(((12 * 2) == 144) && true) --true
+*/
+
+
+
+
+
+// myColor = ["Red", "Green", "White", "Black"];
+// var a = myColor.join(",");
+// console.log(a);
+// var a = myColor.join("+");
+// console.log(a);
+
+
+
+// myColor = ["Red", "Green", "White", "Black"];
+
+
+/*
+Expected Output : 
+"Red,Green,White,Black"
+"Red+Green+White+Black"
+*/
+
+
+var x = [ 3, 8, 7, 6, 5, -4, -3, 2, 1 ]
+// var a =x.sort();
+// console.log(a);
+
+/*
+Sample Output : -4,-3,1,2,3,5,6,7,8
+*/
+
+
+var x = [1, 2, 3, 4, 5, 6];
+
+var y = [];
+function a (){
+		for (var i = 0;i<x.length; i++) {
+	 		if(x[i]>x[i+1]){
+	 
+			}
+	}
+}
+a();
+console.log(y);
+
+
+
+
+// function getSum(total, num) {
+//     return total + num;
+// }
+// function myFunction(item) {
+//     console.log(array = array.reduce(getSum));
+// }
+// myFunction();
+// x=0;
+// function ad(hesab){
+// x=x+hesab
+// }
+// for(i=0;i<array.length;i++){
+// 	ad(array[i]);
+// }
+// console.log(x);
+
+
+
+ /*
+Expected Output : 21 
+*/
+
+
+
+
+// var student = { 
+// name : "David Rayy", 
+// sclass : "VI", 
+// rollno : 12 };
+
+// var new student
+
+// console.log(student.name+","+student.sclass+","+student.rollno);
+
+/*
+Sample Output : name,sclass,rollno
+
+*/
+
+
+
+
+
+
+/*
+
+Example x = 32243;
+Expected Output : 34223	
+*/
+
+// var x = [3,2,2,4,3];
+// var y = []
+// function a (){
+// 	for (var i = 0;i<x.length; i++) {
+// 	y[i]=x[x.length-i-1];
+// }
+// }
+// a();
+// console.log(y);
+
+// var x=[3,2,2,4,3];
+
+// x.sort(function(a, b){return b=a;});
+// console.log(x.join(""));
+
+
+
+
+/*
+Example string : 'the quick brown fox' 
+Expected Output : 'The Quick Brown Fox '
+*/
+
+// var example = 'the quick brown fox';
+
+
+
+// var a = [];
+// for (var i =0;i<example.length; i++) {
+// 	if (example[i]=" ") {
+// 		example[i]=example[i+1];
+// 		console.log(example.toUpperCase());
+// 	}
+
+// }
+
+
+/* 
+
+Example string: Ttttrevaaaniannnn;
+Expected Output : 4t,3a,4n
+
+*/
+//var a=["Ttttrevaaaniannnn"];
+
+
